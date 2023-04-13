10ad29035e09beca16292fd557e4a104d83a49a0
NastassiyaKh <nholodok@yandex.ru>
2023-04-13 21:38:19 +0300

    add Driver class

---
+++ by.itacademy.holodokanastasia/src/main/java/driver/Driver.java
@@ -0,0 +1,30 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Driver {
+    private static WebDriver driver;
+
+    private Driver() {
+    }
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--remote-allow-origins=*");
+            driver = new ChromeDriver(chromeOptions);
+            driver.manage().window().maximize();
+            driver.manage().deleteAllCookies();
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
