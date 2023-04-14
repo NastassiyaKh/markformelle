1dfc049b1d44251e60d78282a6d1ea5f8b6c3bed
NastassiyaKh <nholodok@yandex.ru>
2023-04-14 15:05:21 +0300

    add BaseStep class

--- by.itacademy.holodokanastasia/src/main/java/steps/BaseStep.java
+++ by.itacademy.holodokanastasia/src/main/java/steps/BaseStep.java
@@ -1,10 +1,11 @@
 package steps;
 
+import pages.CartPage;
 import pages.LoginPage;
 import pages.SearchPage;
 
 public class BaseStep {
     LoginPage loginPage = new LoginPage();
     SearchPage searchPage = new SearchPage();
-    CartStep cartStep = new CartStep();
+    CartPage cartPage = new CartPage();
 }
